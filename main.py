b6c9967410f493af0e761f0b7021fe50cf087ae9
YasinRezvani <yasinrezvani2025@gmail.com>
2021-07-29 12:50:24 +0430

    requests to API_key amd get json

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 ﻿import requests
 from pprint import pprint
+
+API_key = "9fed8863a2be30ce1cbecb13f6b57d0b"
+city = input("Enter the City: ")
+
+url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_key+"&lang = fa"
+
+data = requests.get(url).json()
+
+pprint(data)
