ac59d4e905c020bfdebc31b1e317503e283c0650
YasinRezvani <yasinrezvani2025@gmail.com>
2021-09-15 09:49:49 +0430

    add (try,except) prevent to error

--- Simple_Weather_Information.py
+++ Simple_Weather_Information.py
@@ -1,27 +1,32 @@
 ﻿import requests
 from beautifultable import  BeautifulTable
-table = BeautifulTable()
-API_key = "9fed8863a2be30ce1cbecb13f6b57d0b"
-city = input("\nEnter the City: ")
-url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_key+"&lang = fa"
-data = requests.get(url).json()
+try: 
+    table = BeautifulTable()
+    API_key = "9fed8863a2be30ce1cbecb13f6b57d0b"
+    city = input("\nEnter the City: ")
+    url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+API_key+"&lang = fa"
+    data = requests.get(url).json()
 
-weather = data["weather"][0]["description"]
-wind_speed = str(data["wind"]["speed"]) + " m/s"
-temp = str(round(data["main"]["temp"] - 273.15)) + " °C"
-humidity = str(data["main"]["humidity"]) + " %"
-timezone = str(data["timezone"] / 3600) + " h"
+    weather = data["weather"][0]["description"]
+    wind_speed = str(data["wind"]["speed"]) + " m/s"
+    temp = str(round(data["main"]["temp"] - 273.15)) + " °C"
+    humidity = str(data["main"]["humidity"]) + " %"
+    timezone = str(data["timezone"] / 3600) + " h"
 
-print("")
-table.columns.header = [city.title()]
-table.rows.append([weather])
-table.rows.append([temp])
-table.rows.append([humidity])
-table.rows.append([timezone])
-table.rows.append([wind_speed])
-table.rows.header = ["Clouds:", "Temperature:", "Humidity:", "Time Zone:" , "Wind Speed:"]
-table.set_style(BeautifulTable.STYLE_BOX_ROUNDED)
-print(table)
-input()
+    print("")
+    table.columns.header = [city.title()]
+    table.rows.append([weather])
+    table.rows.append([temp])
+    table.rows.append([humidity])
+    table.rows.append([timezone])
+    table.rows.append([wind_speed])
+    table.rows.header = ["Clouds:", "Temperature:", "Humidity:", "Time Zone:" , "Wind Speed:"]
+    table.set_style(BeautifulTable.STYLE_BOX_ROUNDED)
+    print(table)
+    input()
+except:
+    print("")
+    print("-- City Not Found , Invalid Mode --")
+    input()
 
 # Made By Yasin Rezvani 
